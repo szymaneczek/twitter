b316d7b855de0884afcf3f381cc7a7c8cee80da4
Michal Szymanski <szymaneczek@gmail.com>
2019-09-08 10:47:26 +0200

    edycja seleniumTests i przeniesienie geckodrivera do innej lokalizacji

--- src/test/java/pl/sda/twitter/test/SeleniumTests.java
+++ src/test/java/pl/sda/twitter/test/SeleniumTests.java
@@ -1,80 +1,85 @@
 package pl.sda.twitter.test;
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
 
 public class SeleniumTests {
 
-        private WebDriver driver;
-        private String baseUrl;
-        private boolean acceptNextAlert = true;
-        private StringBuffer verificationErrors = new StringBuffer();
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-        @Before
-        public void setUp() throws Exception {
-            driver = new FirefoxDriver();
-            baseUrl = "https://www.katalon.com/";
-            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    @Before
+    public void setUp() {
+        String os = System.getProperty("os.name");
+        if (os.contains("windows")) {
+            System.setProperty("webdriver.gecko.driver", "src/test/geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", "src/test/geckodriver");
         }
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.katalon.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
 
-        @Test
-        public void testSDATWitter() throws Exception {
-            driver.get("http://localhost:8081/twitter/sign-in.jsp");
-            driver.findElement(By.id("inputLogin")).clear();
-            driver.findElement(By.id("inputLogin")).sendKeys("admin");
-            driver.findElement(By.id("inputPassword")).clear();
-            driver.findElement(By.id("inputPassword")).sendKeys("password");
-            driver.findElement(By.id("loginButton")).click();
-            // ERROR: Caught exception [unknown command []]
-        }
+    @Test
+    public void testUntitledTestCase() {
+        driver.get("http://localhost:8081/index.jsp");
+        driver.findElement(By.linkText("Zaloguj się")).click();
+        driver.findElement(By.id("inputLogin")).clear();
+        driver.findElement(By.id("inputLogin")).sendKeys("admin");
+        driver.findElement(By.id("inputPassword")).clear();
+        driver.findElement(By.id("inputPassword")).sendKeys("password");
+        driver.findElement(By.id("loginButton")).click();
+    }
 
-        @After
-        public void tearDown() throws Exception {
-            driver.quit();
-            String verificationErrorString = verificationErrors.toString();
-            if (!"".equals(verificationErrorString)) {
-                fail(verificationErrorString);
-            }
-        }
-
-        private boolean isElementPresent(By by) {
-            try {
-                driver.findElement(by);
-                return true;
-            } catch (NoSuchElementException e) {
-                return false;
-            }
-        }
-
-        private boolean isAlertPresent() {
-            try {
-                driver.switchTo().alert();
-                return true;
-            } catch (NoAlertPresentException e) {
-                return false;
-            }
-        }
-
-        private String closeAlertAndGetItsText() {
-            try {
-                Alert alert = driver.switchTo().alert();
-                String alertText = alert.getText();
-                if (acceptNextAlert) {
-                    alert.accept();
-                } else {
-                    alert.dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
+    @After
+    public void tearDown() {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
         }
     }
 
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
